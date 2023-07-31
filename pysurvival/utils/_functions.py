020e94cadf195d9e1251c2ddfbd4c7e00241a8c2
昌changＣ <91516060+lyc2927@users.noreply.github.com>
2023-07-31 17:23:11 +0800

    Update _functions.py

--- pysurvival/utils/_functions.py
+++ pysurvival/utils/_functions.py
@@ -4,6 +4,6 @@ def __bootstrap__():
     __file__ = pkg_resources.resource_filename(__name__, '_functions.cp37-win_amd64.pyd')
     __loader__ = None; del __bootstrap__, __loader__
     spec = importlib.util.spec_from_file_location(__name__,__file__)
-    mod = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(mod)
+    #mod = importlib.util.module_from_spec(spec)
+    #spec.loader.exec_module(mod)
 __bootstrap__()
